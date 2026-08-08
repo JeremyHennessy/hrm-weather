098bb0c4fdd37e383b48c3cf423cb4f5ad8fc606
Jeremy Hennessy <jeremy.hennessy@gmail.com>
2026-08-08 14:02:57 -0300

    Add PWA service worker

---
+++ sw.js
@@ -0,0 +1,9 @@
+const CACHE='hrm-weather-v3';
+const ASSETS=['./','./index.html','./manifest.webmanifest'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
+self.addEventListener('fetch',e=>{
+  const u=new URL(e.request.url);
+  if(u.hostname.includes('open-meteo.com')) return;
+  e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)));
+});
