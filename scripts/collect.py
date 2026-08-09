c2e0eb0f99d1471cacbeb9b604da936d8519f926
Jeremy Hennessy <jeremy.hennessy@gmail.com>
2026-08-09 14:38:29 -0300

    Install solar context collector

--- scripts/collect.py
+++ scripts/collect.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """Compatibility entry point for the Weather Consensus hourly collector."""
 from eccc_observation_mesh_v2 import install as install_eccc_observation_mesh
+from solar_context_v2 import install as install_solar_context
 
 install_eccc_observation_mesh()
+install_solar_context()
 
 from accuracy_engine_v2_runner import main as run_v2
 from accuracy_engine_v3_publish import main as run_v3
